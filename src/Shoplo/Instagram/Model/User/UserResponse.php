78e9b2fb94b37612fc0f29a4eadfea9f759200f8
Piotr Antosik <mail@piotrantosik.com>
2019-12-04 13:05:19 +0100

    Add user resource

---
+++ src/Shoplo/Instagram/Model/User/UserResponse.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shoplo\Instagram\Model\User;
+
+class UserResponse
+{
+    /**
+     * @var string
+     */
+    public $id;
+
+    /**
+     * @var string|null
+     */
+    public $biography;
+}
---
+++ src/Shoplo/Instagram/Resource/UserResource.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shoplo\Instagram\Resource;
+
+use Shoplo\Instagram\InstagramClient;
+use Shoplo\Instagram\Model\User\UserResponse;
+
+class UserResource
+{
+    private $instagramClient;
+
+    public function __construct(InstagramClient $InstagramClient)
+    {
+        $this->instagramClient = $InstagramClient;
+    }
+
+    private function getUserUrl(string $accountId): string
+    {
+        return \sprintf(
+            '%s?fields=id,biography',
+            $accountId
+        );
+    }
+
+    public function getUser(string $accountId): UserResponse
+    {
+        return $this->instagramClient->get(
+            UserResponse::class,
+            $this->getUserUrl($accountId)
+        );
+    }
+}
